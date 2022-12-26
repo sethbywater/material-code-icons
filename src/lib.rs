d2795fc587c242032d7587459c726019f8950c2a
Seth Bywater <seth@sethbywater.com>
2022-12-26 10:25:29 -0500

    Documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+//! This crate uses the icons from the
+//! [VS Code Material Icon Theme](https://marketplace.visualstudio.com/items?itemName=Equinusocio.vsc-material-theme)
+//! and makes them available in your rust code.
+//!
+//! The main point of interaction in this crate is [`code_icon()`](code_icon).
+//! It takes a file name, folder name, or file extension and returns an SVG as a
+//! `&[u8]`. If you know which icon you want ahead of time, you can also refer
+//! to it specifically.
+//!
+//! ```rust
+//! # use material_code_icons::code_icon;
+//! let icon_from_extension = code_icon("rs");
+//! let icon_from_file_name = code_icon(".gitignore");
+//! let icon_from_file_name_extensions = code_icon("lib.rs");
+//! let icon_from_folder_name = code_icon("src");
+//! let icon = material_code_icons::RUST;
+//! ```
+//!
+//! Currently, this crate does not provide any other return types, and you need
+//! to deserialize the SVG with another crate, but if there is a popular SVG or
+//! image type in Rust then it is possible to add support for it if you open an
+//! issue.
+
 use std::collections::HashMap;
 
 mod icons;
@@ -17,6 +40,14 @@ lazy_static::lazy_static! {
 
 /// Returns an SVG image (as `&'static [u8]`). `name` can be a file name, folder
 /// name, or file extenstion.
+/// ```rust
+/// # use material_code_icons::code_icon;
+/// let icon_from_file_name_extension = code_icon("lib.rs");
+/// let icon_from_extension = code_icon("rs");
+/// let icon_from_file_name = code_icon(".gitignore");
+/// let icon_from_folder_name = code_icon("src");
+///
+/// ```
 pub fn code_icon(name: &str) -> Option<&'static [u8]> {
     let name = &name.to_string();
     println!("{name}");
